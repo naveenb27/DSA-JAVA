bbf23e60823841b3894bcfaacc3e61d7f023009e
Naveen B <99388077+naveenb27@users.noreply.github.com>
2023-09-06 18:08:34 +0530

    Create DFS.java
    Depth-first search and graph implementation.

---
+++ DFS.java
@@ -0,0 +1,72 @@
+import java.util.*;
+import java.util.LinkedList;
+
+class Graph{
+    ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+    Graph(int v){
+        for(int i = 0; i<v; i++){
+            arr.add(new ArrayList<>());
+        }
+    }
+    public void addEdge(int u, int v){
+        arr.get(u).add(v);
+        arr.get(v).add(u);
+    }
+    public void display(){
+        for(int i = 0; i<arr.size(); i++){
+            System.out.println("Vertex "+ i);
+            for(int j = 0; j<arr.get(i).size(); j++){
+                System.out.print(arr.get(i).get(j)+" ");
+            }
+            System.out.println();
+        }
+    }
+    public void BFS(int v){
+        boolean[] visit = new boolean[arr.size()];
+        Queue<Integer> q = new LinkedList<>();
+        q.add(v);
+
+        visit[v] = true;
+        while(q.size() != 0) {
+            int ver = q.remove();
+            System.out.print(ver + " ");
+            for (int i = 0; i < arr.get(ver).size(); i++) {
+                if (!visit[arr.get(ver).get(i)]) {
+                    q.add(arr.get(ver).get(i));
+                    visit[arr.get(ver).get(i)] = true;
+                }
+            }
+        }
+    }
+    public void dfs(int val){//1
+        boolean[] visit = new boolean[arr.size()];
+        dfs_traversal(val, visit);
+    }
+
+    public void dfs_traversal(int val, boolean[] visit){
+        visit[val] = true;//visit
+        System.out.print(val+ " ");
+//        System.out.print("vertex");
+        for(int i = 0; i<arr.get(val).size(); i++){
+            if(!visit[arr.get(val).get(i)]){
+                dfs_traversal(arr.get(val).get(i), visit);
+            }
+        }
+    }
+}
+
+public class DFS{
+    public static void main(String args[]){
+        Graph g = new Graph(5);
+        g.addEdge(1, 2);
+        g.addEdge(1, 4);
+        g.addEdge(2, 3);
+        g.addEdge(2, 4);
+        g.addEdge(3, 4);
+        g.display();
+        System.out.println("BFS");
+        g.BFS(1);
+        System.out.println("\nDFS");
+        g.dfs(1);
+    }
+}
