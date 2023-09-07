48b7e80de6a9f095eb428e9e2d16f16cc5531472
Naveen B <99388077+naveenb27@users.noreply.github.com>
2023-09-07 06:28:17 +0530

    Update BinarySearchTree.java

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -19,14 +19,14 @@ class BTS{
         }else{
             Node temp = root;
             while(true){
-                if(temp.val > n.val){
+                if(temp.val < n.val){
                     if(temp.right == null){
                         temp.right = n;
                         break;
                     }else{
                         temp = temp.right;
                     }
-                }else if(temp.val < n.val){
+                }else if(temp.val > n.val){
                     if(temp.left == null){
                         temp.left = n;
                         break;
